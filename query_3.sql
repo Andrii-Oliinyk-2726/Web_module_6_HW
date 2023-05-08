de98cf78911c51640da91ead64fad6e508dcc8ec
Andrii-Oliinyk-2726 <123332365+Andrii-Oliinyk-2726@users.noreply.github.com>
2023-05-09 01:09:01 +0300

    Create query_3.sql

---
+++ query_3.sql
@@ -0,0 +1,9 @@
+-- 3. Найти средний балл в группах по определенному предмету.
+SELECT d.name, gr.name , ROUND(AVG(g.grade), 2) AS average_grade
+FROM grades g 
+JOIN students s ON s.id = g.student_id 
+JOIN disciplines d  ON d.id = g.discipline_id
+JOIN [groups] gr ON gr.id = s.group_id 
+WHERE d.id = 7
+GROUP BY gr.name, d.name  
+ORDER BY average_grade DESC;
