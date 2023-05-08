abd01161151fe2db72710579b76cb6feac96258f
Andrii-Oliinyk-2726 <123332365+Andrii-Oliinyk-2726@users.noreply.github.com>
2023-05-09 01:08:51 +0300

    Create query_1.sql

---
+++ query_1.sql
@@ -0,0 +1,7 @@
+-- 1. Найти 5 студентов с наибольшим средним баллом по всем предметам.
+SELECT s.fullname, ROUND(AVG(g.grade), 2) AS average_grade
+FROM grades g 
+JOIN students s ON s.id = g.student_id 
+GROUP BY s.fullname 
+ORDER BY average_grade DESC 
+LIMIT 5;
